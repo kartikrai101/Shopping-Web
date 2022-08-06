db70997811d041c19f84cdbbefa3ab8be066ddd3
Kartik Rai <78352715+kartikrai101@users.noreply.github.com>
2022-08-06 23:53:54 +0530

    Revert "made changes to the product routes and controller functions"

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,9 @@
 const express = require("express"); // importing the express package
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { getAllProducts } = require("../controllers/productController");
 
 const router = express.Router(); // setting up the router method from the express library
 
 
-router.route("/products").get(getAllProducts);  // GET 
-router.route("/products/new").post(createProduct);  // POST
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);  // PUT & DELETE & GET
-
+router.route("/products").get(getAllProducts);
 
 module.exports = router;
